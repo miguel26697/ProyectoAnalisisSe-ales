a7cea3dcb2c6735cbd933109e08daa04af2edfd3
miguel26697 <64111348+miguel26697@users.noreply.github.com>
2020-11-01 16:37:29 -0500

    Add files via upload

--- covid.sql
+++ covid.sql
@@ -2,22 +2,24 @@ drop database if exists covid;
 create database covid;
 use covid;
 
-create table casos(
-id_casos varchar(15) primary key,
-numCasos int,
-numCasosHoy int,
-numHospi int,
-numHospiHoy int,
-numFalle int,
-numFalleHoy int,
-numCasa int,
-numCasaHoy int, 
-numRecupe int,
-numRecupeHoy int,
-numUci int,
-numUciHoy int);
-
-
-select * from casos where id_casos = "2020-09-29";
-
-select * from casos
+create table total( 
+fecha varchar(100)  primary key, 
+Casos varchar(30), 
+Recuperados varchar(30),
+ Hospitalizados varchar(30),
+ Unidades varchar(30), 
+ Muertes varchar(30)); 
+ 
+ create table localidades( 
+ fecha varchar(100),
+ nombre varchar(30), 
+ casos varchar(30));
+ 
+ create table depa( 
+ fecha varchar(100),
+ nombre varchar(30), 
+ casos varchar(30));
+ 
+Select * from localidades;
+Select * from depa;
+Select * from total;
